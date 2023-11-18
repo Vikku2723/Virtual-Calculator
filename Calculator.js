222e1ae6677de02c102ed5275b704bd701bbc218
Vikku2723 <131509866+Vikku2723@users.noreply.github.com>
2023-11-18 22:48:26 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,20 @@
+// function sum(){
+//     let d=Number(inputbox.value)+Number(inputbox.value)
+//     h1.innerHTML=d
+// }
+// function sub(){
+//     let d=Number(inputbox.value)-Number(inputbox.value)
+//     h1.innerHTML=d
+// }
+// function mul(){
+//     let d=Number(inputbox.value)*Number(inputbox.value)
+//     h1.innerHTML=d
+// }
+// function div(){
+//     let d=Number(inputbox.value)/Number(inputbox.value)
+//     h1.innerHTML=d
+// }
+// function mod(){
+//     let d=Number(inputbox.value)%Number(inputbox.value)
+//     h1.innerHTML=d
+// }
